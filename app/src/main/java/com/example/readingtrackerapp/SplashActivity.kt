4dc017bac54a6f6a8ab064c50a17bf36fe895903
lef8 <alan.sipiorski14@gmail.com>
2023-04-27 21:16:09 +0200

    add splash activity

---
+++ app/src/main/java/com/example/readingtrackerapp/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.readingtrackerapp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1000)
+    }
+}
